92190f1461eba8983f5f00c305bad398a5067733
Ziad El-Sayed <zelsydd@gmail.com>
2026-07-09 20:28:19 +0300

    Remove Repositories

--- Repositories/IUnitOfWork.cs
+++
@@ -1,8 +0,0 @@
-namespace Repositories
-{
-  // to avoid exposing DbContext in services
-  public interface IUnitOfWork
-  {
-    Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default);
-  }
-}
